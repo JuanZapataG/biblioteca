ec736ffe500861ac66a916980d9d9906e12c65c3
Juan David Zapata Garcia <juadazap@bancolombia.com.co>
2023-12-12 10:27:22 -0500

    chore(Base de Datos): Se agrega la configuracion de la Base de datos

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS autores (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(255),
+    nacionalidad VARCHAR(255),
+    fechaNacimiento VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS libros (
+    id INT PRIMARY KEY,
+    titulo VARCHAR(255),
+    isbn VARCHAR(255),
+    autor_id INT,
+    pesoNeto DOUBLE,
+    precioBase DOUBLE,
+    editorial VARCHAR(255),
+    FOREIGN KEY (autor_id) REFERENCES autores(id)
+);
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(255),
+    email VARCHAR(255),
+    estado VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS prestamos (
+    id INT PRIMARY KEY,
+    usuario_id INT,
+    libro_id INT,
+    fecha_prestamo DATE,
+    fecha_devolucion DATE,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (libro_id) REFERENCES libros(id)
+);
